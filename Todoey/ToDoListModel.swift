72cfd4f9c393c26aa8fe280566e22e358eadc875
Andrew Denaro <ibdenaro@gmail.com>
2018-11-29 06:56:41 -0500

    Convert to MVC design pattern.

---
+++ Todoey/ToDoListModel.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoListModel.swift
+//  Todoey
+//
+//  Created by Andrew Denaro on 11/27/18.
+//  Copyright © 2018 Andrew Denaro. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
